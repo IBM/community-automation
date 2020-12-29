7b29a16c1237b3fb4af89bdedfc31d664981b73b
Walter Krapohl <walterkrapohl@Walters-MacBook-Pro.local>
2020-12-29 16:19:02 -0600

    Cleanup

--- ansible/roles/request-ocs-fyre/files/00.check-cpus.sh
+++ ansible/roles/request-ocs-fyre/files/00.check-cpus.sh
@@ -4,7 +4,6 @@ my_dir=$(dirname $(readlink -f $0))
 
 yum install jq -y > /dev/null
 CPUS=$(oc get node -o json | jq -r '.items[] | select(.metadata.labels["node-role.kubernetes.io/worker"] != null) | .status.capacity.cpu' | awk '{s+=$1} END {print s}')
-echo "Total CPUs is $CPUS"
 if [ $CPUS -lt 48 ]; then
   echo "OCS requires 48 cpus and your cluster only has ${CPUS}"
   echo "If the cluster uses machinesets you can scale up all machinesets using:"
