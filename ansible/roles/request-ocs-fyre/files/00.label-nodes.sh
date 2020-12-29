e7e05138022a1a7ed898f1975af9560e8d90ea79
Walter Krapohl <walterkrapohl@Walters-MacBook-Pro.local>
2020-12-28 19:24:53 -0600

    Use more set_fact

--- ansible/roles/request-ocs-fyre/files/00.label-nodes.sh
+++ ansible/roles/request-ocs-fyre/files/00.label-nodes.sh
@@ -2,7 +2,7 @@
 
 my_dir=$(dirname $(readlink -f $0))
 
-yum install jq -y
+yum install jq -y > /dev/null
 CPUS=$(oc get node -o json | jq -r '.items[] | select(.metadata.labels["node-role.kubernetes.io/worker"] != null) | .status.capacity.cpu' | awk '{s+=$1} END {print s}')
 echo "Total CPUs is $CPUS"
 if [ $CPUS -lt 48 ]; then
