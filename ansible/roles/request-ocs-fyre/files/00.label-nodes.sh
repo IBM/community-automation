3d79ed945847083ae2904dd6b220e6b8f4dd90cf
Walter Krapohl <walterkrapohl@Walters-MacBook-Pro.local>
2020-12-28 18:05:18 -0600

    Check CPUs

--- ansible/roles/request-ocs-fyre/files/00.label-nodes.sh
+++ ansible/roles/request-ocs-fyre/files/00.label-nodes.sh
@@ -3,13 +3,13 @@
 my_dir=$(dirname $(readlink -f $0))
 
 CPUS=$(oc get node -o json | jq -r '.items[] | select(.metadata.labels["node-role.kubernetes.io/worker"] != null) | .status.capacity.cpu' | awk '{s+=$1} END {print s}')
+echo "Total CPUs is $CPUS"
 if [ $CPUS -lt 48 ]; then
   echo "OCS requires 48 cpus and your cluster only has ${CPUS}"
   echo "If the cluster uses machinesets you can scale up all machinesets using:"
   echo "   oc scale machinesets --replicas=3 -n openshift-machine-api --all"
   exit 1
 fi
-
 
 oc_nodes=$(oc get node -o name | grep worker)
 nodes=$(oc get node -o name | grep worker | awk -F'/' '{print $2}')
@@ -19,12 +19,3 @@ for node in $oc_nodes
 do
     oc label $node cluster.ocs.openshift.io/openshift-storage=''
 done
-
-#echo "Persistently enable container use of the Ceph file system in SELinux on all worker nodes."
-##for node in $nodes
-#do
-#    if ! grep $(ssh-keyscan -H $node | awk '{ if ($2=="ssh-rsa")  print $3 }') ~/.ssh/known_hosts > /dev/null 2>&1; then
-#        ssh-keyscan -H $node >> ~/.ssh/known_hosts
-#    fi
-#    ssh core@$node sudo setsebool -P container_use_cephfs on
-#done
