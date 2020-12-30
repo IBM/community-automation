a36de2c35a60f1b2b027739aa7676f31e35b1130
Walter Krapohl <walterkrapohl@Walters-MacBook-Pro.local>
2020-12-29 19:37:04 -0600

    Dynamic set of storageCluster replicas.

--- ansible/roles/request-ocs-fyre/files/00.check-cpus.sh
+++ ansible/roles/request-ocs-fyre/files/00.check-cpus.sh
@@ -4,7 +4,7 @@ my_dir=$(dirname $(readlink -f $0))
 
 yum install jq -y > /dev/null
 CPUS=$(oc get node -o json | jq -r '.items[] | select(.metadata.labels["node-role.kubernetes.io/worker"] != null) | .status.capacity.cpu' | awk '{s+=$1} END {print s}')
-echo "Worker CPUs is total $CPUS."
+echo "Worker CPUs is total $CPUS. "
 if [ $CPUS -lt 48 ]; then
   echo "OCS requires 48 cpus and your cluster only has ${CPUS}"
   echo "If the cluster uses machinesets you can scale up all machinesets using:"
