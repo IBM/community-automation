7746b5c21aa37330c0cd51343e8b106d0aca0eda
Walter Krapohl <walterkrapohl@Walters-MacBook-Pro.local>
2021-01-07 18:15:41 -0600

    Check for jq

--- ansible/roles/request-ocs-local-storage/files/00.check-cpus.sh
+++ ansible/roles/request-ocs-local-storage/files/00.check-cpus.sh
@@ -2,7 +2,6 @@
 
 my_dir=$(dirname $(readlink -f $0))
 
-yum install jq -y > /dev/null
 CPUS=$(oc get node -o json | jq -r '.items[] | select(.metadata.labels["node-role.kubernetes.io/worker"] != null) | .status.capacity.cpu' | awk '{s+=$1} END {print s}')
 echo "Worker CPUs is total $CPUS. "
 if [ $CPUS -lt 48 ]; then
