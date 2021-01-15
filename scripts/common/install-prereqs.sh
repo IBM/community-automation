4c65fa71ca329bd795ee64e9ee3669919fd1ed12
OC Travis <octravis@us.ibm.com>
2021-01-15 13:33:52 -0600

    update dir referennce

--- scripts/common/install-prereqs.sh
+++ scripts/common/install-prereqs.sh
@@ -11,10 +11,11 @@ apt -y install ansible
 # run from script location
 file_location=$(find . -type f -name install-prereqs.sh | grep .) && cd "$(dirname $file_location)" || { echo "could not find install-prereqs.sh"; exit 1; }
 
+play_dir="../../ansible/prereq-play"
 # install ansible modules
-ansible-galaxy collection install -r ../../ansible/prereq-play/requirements.yml
+ansible-galaxy collection install -r $play_dir/requirements.yml
 
 # update and install python libraries
-ansible-playbook -i ../../inventory ../../ansible/prereq-play/prereq-play.yml
+ansible-playbook -i $play_dir/inventory $play_dir/prereq-play.yml
 
 echo "Install and updates complete."
