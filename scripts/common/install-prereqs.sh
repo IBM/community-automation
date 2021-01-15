01ad3eaf186260624f383c63a48621f3f041609b
OC Travis <octravis@us.ibm.com>
2021-01-15 12:12:12 -0600

    added another dir up for play.

--- scripts/common/install-prereqs.sh
+++ scripts/common/install-prereqs.sh
@@ -9,9 +9,9 @@ apt -y update
 apt -y install ansible
 
 # install ansible modules
-ansible-galaxy collection install -r ../../ansible/prereq-play/requirements.yml
+ansible-galaxy collection install -r ../../../ansible/prereq-play/requirements.yml
 
 # update and install python libraries
-ansible-playbook -i inventory ../../ansible/prereq-play/prereq-play.yml
+ansible-playbook -i inventory ../../../ansible/prereq-play/prereq-play.yml
 
 echo "Install and updates complete."
