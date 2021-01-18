df02b597fd505fdf7dd7c90409de7196888dcff8
OC Travis <octravis@us.ibm.com>
2021-01-18 13:47:51 -0600

    update path

--- scripts/common/community-docker.sh
+++ scripts/common/community-docker.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-repo_dir=/workspace/docker-test
+repo_dir="$(pwd)"
 play_dir=/tmp/community-automation/ansible/prereq-play
 
 # start docker container and pass environment variables
 docker run --name ubuntu_bash -v $repo_dir/community-automation:/tmp/community-automation --rm -i -t -d ubuntu sleep 120m
-# update ubuntu
-docker exec -it ubuntu_bash apt update
 # install pre-reqs
-docker exec -it ubuntu_bash apt -y install python3 python3-pip git curl wget sudo jq gpg
-docker exec -it ubuntu_bash pip3 install --upgrade pip
-docker exec -it ubuntu_bash pip3 install ansible
+docker exec -it ubuntu_bash bash -c "apt -y update; apt -y install python3 python3-pip git curl wget sudo jq gpg; pip3 install --upgrade pip; pip3 install ansible"
 docker exec -it ubuntu_bash bash -c "ansible-galaxy collection install -r $play_dir/requirements.yml"
 docker exec -it ubuntu_bash bash -c "ansible-playbook -i $play_dir/inventory $play_dir/docker-play.yml"
 
