8ffc38c6bdffa7e453c2ef476f0c5f9b1cca013a
OC Travis <octravis@us.ibm.com>
2021-01-18 13:19:22 -0600

    cleanup

--- scripts/common/community-docker.sh
+++ scripts/common/community-docker.sh
@@ -2,20 +2,20 @@
 
 repo_dir=/workspace/docker-test
 play_dir=/tmp/community-automation/ansible/prereq-play
-export GITHUB_TOKEN="RAYSGITHUBTOCKEN"
 
 # start docker container and pass environment variables
-docker run -e GITHUB_TOKEN \
-           --name ubuntu_bash -v $repo_dir/community-automation:/tmp/community-automation --rm -i -t -d ubuntu sleep 120m
+docker run --name ubuntu_bash -v $repo_dir/community-automation:/tmp/community-automation --rm -i -t -d ubuntu sleep 120m
 # update ubuntu
 docker exec -it ubuntu_bash apt update
 # install pre-reqs
 docker exec -it ubuntu_bash apt -y install python3 python3-pip git curl wget sudo jq gpg
 docker exec -it ubuntu_bash pip3 install --upgrade pip
 docker exec -it ubuntu_bash pip3 install ansible
+docker exec -it ubuntu_bash bash -c "ansible-galaxy collection install -r $play_dir/requirements.yml"
 docker exec -it ubuntu_bash bash -c "ansible-playbook -i $play_dir/inventory $play_dir/docker-play.yml"
+
 # open docker container to execute plays
 docker exec -it ubuntu_bash bash
 
-echo " stopping container..."
-docker stop ubuntu_bash
+echo "When you exec from docker conatiner, here is how to stop the container"
+echo "docker stop ubuntu_bash"
