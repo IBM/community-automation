86acc461cc4d4ab1d5de0c13757556821ddea655
OC Travis <octravis@us.ibm.com>
2021-01-19 17:24:11 -0600

    remove --rm from docker run

--- scripts/common/community-docker.sh
+++ scripts/common/community-docker.sh
@@ -13,7 +13,7 @@ repo_dir="$(pwd)"
 play_dir=/tmp/community-automation/ansible/prereq-play
 container_name="community_auto_bash"
 # start docker container and pass environment variables
-docker run --name "$container_name" -v "$repo_dir":/tmp/community-automation --rm -i -t -d ubuntu:latest sleep 120m
+docker run --name "$container_name" -v "$repo_dir":/tmp/community-automation -i -t -d ubuntu:latest sleep 120m
 
 # install pre-reqs
 docker exec -it "$container_name" bash -c "apt -y update; apt -y install python3 python3-pip git curl wget sudo jq gpg vim; pip3 install --upgrade pip; pip3 install ansible"
