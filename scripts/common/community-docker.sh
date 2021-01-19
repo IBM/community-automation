cc18abba086843cc216a1ee0f56b921ab25f7fe2
OC Travis <octravis@us.ibm.com>
2021-01-19 13:59:52 -0600

    adding vim to image

--- scripts/common/community-docker.sh
+++ scripts/common/community-docker.sh
@@ -16,7 +16,7 @@ container_name="community_auto_bash"
 docker run --name "$container_name" -v "$repo_dir":/tmp/community-automation --rm -i -t -d ubuntu:latest sleep 120m
 
 # install pre-reqs
-docker exec -it "$container_name" bash -c "apt -y update; apt -y install python3 python3-pip git curl wget sudo jq gpg; pip3 install --upgrade pip; pip3 install ansible"
+docker exec -it "$container_name" bash -c "apt -y update; apt -y install python3 python3-pip git curl wget sudo jq gpg vim; pip3 install --upgrade pip; pip3 install ansible"
 docker exec -it "$container_name" bash -c "ansible-galaxy collection install -r $play_dir/requirements.yml"
 docker exec -it "$container_name" bash -c "ansible-playbook -i $play_dir/inventory $play_dir/prereq-play.yml"
 
