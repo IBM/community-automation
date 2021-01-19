ee606c10298f445e6d09a9da7271e5403c5ab0e5
OC Travis <octravis@us.ibm.com>
2021-01-19 09:58:19 -0600

    update conditionals for OS

--- scripts/common/community-docker.sh
+++ scripts/common/community-docker.sh
@@ -5,32 +5,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-function usage () {
-  echo "When using RHEL 8, you must provide redhat credentials"
-  echo "--redhat_username YOUR_REDHAT_USERNAME"
-  echo "--redhat_password YOUR_REDHAT_PASSWORD"
-}
-
-function rhel8_support () {
-  local num_of_params=4
-
-  [[ $# -lt $num_of_params ]] && { usage ; exit 1; } || true
-
-  # Set the parameters
-  while test $# -gt 0; do
-     [[ $1 =~ ^-u|--redhat_username$ ]] && { redhat_username="$2" ; shift 2; continue; };
-     [[ $1 =~ ^-p|--redhat_password$ ]] && { redhat_password="$2" ; shift 2; continue; };
-     echo "Parameter not recognized: $1, ignored"
-     shift
-  done
-
-  sudo subscription-manager register --username "$redhat_username" --password "$redhat_password"
-  sudo subscription-manager attach --auto
-  sudo yum module install -y container-tools
-  sudo yum install -y podman-docker
-  sudo podman login registry.redhat.io --username "$redhat_username" --password "$redhat_password"
-  echo "[INFO] redhat 8 support complete."
-}
+source rhel8-functions.sh
 
 [[ $(cat /etc/redhat-release | grep '8.') ]] && rhel8_support $@ || true
 
--- scripts/common/install-prereqs.sh
+++ scripts/common/install-prereqs.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# install ansible 2.9+
-
 [[ $(ansible --version | head -1 | awk '{ print $2 }' | cut -d . -f1-2) == 2.10 ]] && { echo "ansible at correct version"; exit 0; } || true
 
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt -y remove --purge ansible
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt -y update
-sudo apt -y install ansible
+[[ $(cat /etc/redhat-release | grep '8.') ]] && rhel8_support $@ || true
+
+[[ $(cat /etc/os-release  | grep NAME | grep Ubuntu | grep -v PRETTY | cut -d \" -f2) == "Ubuntu" ]]  && sudo apt -y update \
+sudo apt -y upgrade \
+sudo apt -y remove --purge ansible || true
+
+# check for python3 version 3.6.9
+[[ $(python3 --version | awk '{ print $2 }') == "3.6.9" ]] && { echo "correct version of python installed"; exit 0; }|| true
+
+# install python3, assumes
+sudo apt -y install python3 python3-pip
+sudo pip3 -y install ansible
 
 # run from script location
 file_location=$(find . -type f -name install-prereqs.sh | grep .) && cd "$(dirname $file_location)" || { echo "could not find install-prereqs.sh"; exit 1; }
+
 
 play_dir="../../ansible/prereq-play"
 # install ansible modules
