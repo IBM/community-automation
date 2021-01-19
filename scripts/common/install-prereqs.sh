de129d0130d8fa556f80fac2b99429218f04aff4
OC Travis <octravis@us.ibm.com>
2021-01-19 12:43:32 -0600

    update grep line

--- scripts/common/install-prereqs.sh
+++ scripts/common/install-prereqs.sh
@@ -11,7 +11,7 @@ ansible_installed=false
 
 if [ $ansible_installed == false ]; then
 # check rhel 8 and update/install
-[[ -f /etc/redhat-release ]] && [[ $(cat /etc/redhat-release | grep '8.') ]] && rhel8_support $@ || true
+[[ -f /etc/redhat-release ]] && [[ $(grep '8.' /etc/redhat-release) ]] && rhel8_support $@ || true
 
 # check ubuntu and update/install
 [[ -f /etc/os-release ]] && [[ $(cat /etc/os-release  | grep NAME | grep Ubuntu | grep -v PRETTY | cut -d \" -f2) == "Ubuntu" ]] && { sudo apt -y update; \
