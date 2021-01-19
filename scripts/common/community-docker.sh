93f4ee5f68b70b4c8a7737bd40bfa20a2c7c1dc2
OC Travis <octravis@us.ibm.com>
2021-01-19 08:54:04 -0600

    added support for RHEL 8

--- scripts/common/community-docker.sh
+++ scripts/common/community-docker.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 
+# bash best practices
+set -o errexit
+set -o nounset
+set -o pipefail
+
+function usage () {
+  echo "When using RHEL 8, you must provide redhat credentials"
+  echo "--redhat_username YOUR_REDHAT_USERNAME"
+  echo "--redhat_password YOUR_REDHAT_PASSWORD"
+}
+
+function rhel8_support () {
+  local num_of_params=4
+
+  [[ $# -lt $num_of_params ]] && { usage ; exit 1; } || true
+
+  # Set the parameters
+  while test $# -gt 0; do
+     [[ $1 =~ ^-u|--redhat_username$ ]] && { redhat_username="$2" ; shift 2; continue; };
+     [[ $1 =~ ^-p|--redhat_password$ ]] && { redhat_password="$2" ; shift 2; continue; };
+     echo "Parameter not recognized: $1, ignored"
+     shift
+  done
+
+  sudo subscription-manager register --username "$redhat_username" --password "$redhat_password"
+  sudo subscription-manager attach --auto
+  sudo yum module install -y container-tools
+  sudo yum install -y podman-docker
+  sudo podman login registry.redhat.io --username "$redhat_username" --password "$redhat_password"
+  echo "[INFO] redhat 8 support complete."
+}
+
+[[ $(cat /etc/redhat-release | grep '8.') ]] && rhel8_support $@ || true
+
 repo_dir="$(pwd)"
 play_dir=/tmp/community-automation/ansible/prereq-play
 container_name="community_auto_bash"
 # start docker container and pass environment variables
-docker run --name "$container_name" -v "$repo_dir":/tmp/community-automation --rm -i -t -d ubuntu sleep 120m
+docker run --name "$container_name" -v "$repo_dir":/tmp/community-automation --rm -i -t -d ubuntu:latest sleep 120m
 
 # install pre-reqs
 docker exec -it "$container_name" bash -c "apt -y update; apt -y install python3 python3-pip git curl wget sudo jq gpg; pip3 install --upgrade pip; pip3 install ansible"
