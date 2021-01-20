77ba77070e12462e86fd4c38170b0f5dbf55a6e2
OC Travis <octravis@us.ibm.com>
2021-01-19 19:57:34 -0600

    update conditional

--- scripts/common/install-prereqs.sh
+++ scripts/common/install-prereqs.sh
@@ -7,7 +7,7 @@ set -o pipefail
 
 ansible_installed=false
 # check for ansible version
-[[ $(which ansible) ]] && [[ $(ansible --version | head -1 | awk '{ print $2 }' | cut -d . -f1-2) == 2.10 ]] && { echo "ansible at correct version"; ansible_install=true ; } || true
+[[ $(which ansible) ]] && [[ $(ansible --version | head -1 | awk '{ print $2 }' | cut -d . -f1-2) == "2.10" ]] && { echo "ansible at correct version"; ansible_install=true ; } || true
 
 if [ $ansible_installed == false ]; then
 # check rhel 8 and update/install
