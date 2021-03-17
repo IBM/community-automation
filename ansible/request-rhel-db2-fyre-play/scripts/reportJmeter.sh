b43136dadf0b66c650e6b4afefc1be4bff300e5c
Steven Schder <schader@us.ibm.com>
2021-03-17 12:46:17 -0400

    testing mode on scripts dir copy

--- ansible/request-rhel-db2-fyre-play/scripts/reportJmeter.sh
+++
@@ -1,8 +0,0 @@
-# presume 1 report jmeter.jtl input file
-jreportCount=$(ls -1 *.jmeter.jtl|wc -l)
-[ "$jreportCount" != "1" ] && echo "ERROR: multiple input .jmeter.jtl files" && exit 2||true
-IFS='.' read inputjreport jreportext <<< $(ls -1 *.jmeter.jtl) 
-[ -z $inputjreport ] && exit 1
-### run as a sep process on a large host
-### jmeter -g $inputjmx.jmeter.file -o $inputjmx.jmeter.report
-jmeter -g $inputjreport.$jreportext -o $inputjreport.jmeter.report
