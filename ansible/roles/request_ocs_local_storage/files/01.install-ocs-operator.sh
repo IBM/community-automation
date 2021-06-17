533587508183ce7a2c055644aebba8de626e4547
Christopher Webster <WEBSTEC1@uk.ibm.com>
2021-06-17 11:41:04 +0100

    Making Install Operator Check More Specific

--- ansible/roles/request_ocs_local_storage/files/01.install-ocs-operator.sh
+++ ansible/roles/request_ocs_local_storage/files/01.install-ocs-operator.sh
@@ -17,7 +17,7 @@ sleep 5
 oc project openshift-storage
 ###
 csv_counter=30
-oc get csv --no-headers=true
+oc get csv local-storage-operator.4.6.0-202106021513 --no-headers=true | grep local-storage-operator
 rc_csv=$?
 echo -n "Checking for ocs operator csv "
 while [[ $rc_csv -eq 1 ]]
@@ -36,8 +36,8 @@ done
 echo
 echo -n "Waiting for Openshift Container Storage Operator to be ready."
 COUNTER=60
-STATUS=$(oc get csv --no-headers=true | rev | cut -f1 -d' ' | rev | tr -d ' ')
-while [[ "${STATUS}" != *"Succeeded"* ]]
+STATUS=$(oc get csv --no-headers=true | grep local-storage-operator | rev | cut -f1 -d' ' | rev | tr -d ' ')
+while [[ "${STATUS}" != "Succeeded" ]]
 do
     COUNTER=$(( ${COUNTER} -1 ))
     if [ "$COUNTER" -lt 1 ]
@@ -46,7 +46,7 @@ do
         exit 1
     fi
     sleep 10
-    STATUS=$(oc get csv --no-headers=true | rev | cut -f1 -d' ' | rev | tr -d ' ')
+    STATUS=$(oc get csv --no-headers=true | grep local-storage-operator | rev | cut -f1 -d' ' | rev | tr -d ' ')
     echo -n $STATUS
     echo -n .
 done
