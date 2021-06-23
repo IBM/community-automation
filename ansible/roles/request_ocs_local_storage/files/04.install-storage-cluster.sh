b1ae71b05aba884365a9c89cdcb27b0d20652c87
Christopher Webster <WEBSTEC1@uk.ibm.com>
2021-06-23 09:29:15 +0100

    Changing to await default storage class to become available

--- ansible/roles/request_ocs_local_storage/files/04.install-storage-cluster.sh
+++ ansible/roles/request_ocs_local_storage/files/04.install-storage-cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 my_dir=$(dirname $(readlink -f $0))
-
+default_sc=$1
 echo "Creating Storage Cluster."
 
 oc apply -f $my_dir/04.storage-cluster.yaml
@@ -13,19 +13,19 @@ then
 fi
 
 # check if storage classes have been created
-echo "Checking for noobaa Storage Class"
+echo "Checking for $default_sc Storage Class"
 scCounter=0
 rc=1
 until [ $rc -eq 0 ]
 do
-  echo "Waiting for noobaa storageclass to come up $(date)"
+  echo "Waiting for $default_sc storageclass to come up $(date)"
   sleep 10
   ((scCounter ++))
   if [ $scCounter -eq 60 ]; then
-   echo "noobaa storageclass never became available."
+   echo "$default_sc storageclass never became available."
    exit 1
   fi
-  oc get sc --no-headers | cut -f1 -d' ' | grep noobaa >/dev/null
+  oc get sc --no-headers | cut -f1 -d' ' | grep $default_sc >/dev/null
   rc=$?
 done
 
