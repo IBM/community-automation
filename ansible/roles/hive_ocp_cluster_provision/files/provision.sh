8e451d27b27d06676148b91232e1284596a09ea3
OC Travis <octravis@us.ibm.com>
2021-09-15 16:01:52 -0500

    check for new podname

--- ansible/roles/hive_ocp_cluster_provision/files/provision.sh
+++ ansible/roles/hive_ocp_cluster_provision/files/provision.sh
@@ -10,11 +10,16 @@ CLUSTER_NAME=$1
 deploy_log=/tmp/$CLUSTER_NAME.log
 retries=120
 bootstrap_complete=0
+previous_podname=""
+provision_podname=""
 
 # process deployment and check for success
 for (( i=0; i <= retries; i++ )); do
    # get provising name
+   previous_podname=$provision_podname 
    provision_podname=$(oc --no-headers=true get pods -n "$CLUSTER_NAME" -l hive.openshift.io/job-type=provision,hive.openshift.io/cluster-deployment-name="$CLUSTER_NAME" -o name | cut -d / -f2)
+   # check to see if install failed and has restarted new pod.
+   [[ $provision_podname != "$previous_podname" ]] && bootstrap_complete=0 || true  
    oc logs -n "$CLUSTER_NAME" "$provision_podname" -c hive > "$deploy_log"
    grep "Bootstrap failed to complete" "$deploy_log" && { echo "Bootstrap failed"; exit 1; } || true
    [[ $bootstrap_complete != 1 ]] && { grep -i "Bootstrap status: complete" "$deploy_log" && { echo "bootstrap complete"; bootstrap_complete=1; } } || { grep -i "install completed successfully" "$deploy_log" && exit 0 || continue ; }
