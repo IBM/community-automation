5a163d72e74de11062e01bba0d1b63022122c9fc
OC Travis <octravis@us.ibm.com>
2021-10-14 10:32:20 -0500

    minor updates to script

--- ansible/roles/cleanup_dhcp_leases/files/remove-leases.sh
+++ ansible/roles/cleanup_dhcp_leases/files/remove-leases.sh
@@ -11,6 +11,7 @@ detail_file="$cluster_name"_details.log
 
 rm -f $temp_lease_file
 
+echo "Stop dhcp server..."
 systemctl stop isc-dhcp-server.service
 
 while IFS= read -r ip || [[ -n "$ip" ]]; do
@@ -22,8 +23,9 @@ while IFS= read -r ip || [[ -n "$ip" ]]; do
      [[ $remove == "true" ]] && { grep -q "}" <<< $line && { remove=false; continue; } || continue; } || printf '%s\n' "$line" >> "$temp_lease_file"
    done < "$lease_file"
 
-cp "$temp_lease_file" "$lease_file"
-rm -f "$temp_lease_file"
+   cp "$temp_lease_file" "$lease_file"
+   rm -f "$temp_lease_file"
 done < "$detail_file"
 
+echo "Start dhcp server..."
  systemctl start isc-dhcp-server.service
