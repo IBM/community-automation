bb02418600b825dc373f904e18fb0db28a39989d
Steven Schder <schader@us.ibm.com>
2022-01-28 10:27:54 -0500

    start cluster

---
+++ ansible/roles/twas_cell_start_cluster/files/twas_cell_start_cluster.py
@@ -0,0 +1,42 @@
+# wsadminlib.py is needed
+#https://github.com/wsadminlib/wsadminlib/blob/master/bin/wsadminlib.py
+wsadminlib = "wsadminlib.py"
+execfile(wsadminlib)
+
+#---------------------------------------------
+# Check/Print Usage
+#---------------------------------------------
+
+def printUsageAndExit (  ):
+        print ("Usage: wsadmin -lang jython -f twas_cell_start_cluster.py cluster <cluster1>")
+        print ("or:")
+        print ("Usage: wsadmin -lang jython -f twas_cell_start_cluster.py cluster <cluster1> <cluster2>")
+        sys.exit()
+#endDef 
+
+#---------------------------------------------
+# Parse command line arguments
+#---------------------------------------------
+
+print ("Parsing command line arguments...")
+
+clusterNamesList = []
+if (len(sys.argv) < 2):
+        printUsageAndExit( )
+else:
+        scope = sys.argv[0]
+        print ("Scope:   "+scope)
+        if (scope == "cluster"):
+                clusterNamesList.append(sys.argv[1])
+               	print ("Cluster: "+clusterNamesList[0])
+                if ( len(sys.argv) == 3 ):
+                        clusterNamesList.append(sys.argv[2])
+                        print ("Cluster: "+clusterNamesList[1])
+        else:
+                print ("Error: Invalid Argument ("+scope+")")
+                printUsageAndExit( )
+        #endElse 
+#endElse 
+
+for clusterName in clusterNamesList:
+        startCluster( clusterName )
