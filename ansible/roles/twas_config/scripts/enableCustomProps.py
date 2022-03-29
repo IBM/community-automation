488da224bec0a06ac9b46fda6e40e5de5ce49433
Steven Schder <schader@us.ibm.com>
2022-03-29 18:34:31 -0400

    more custom pen test options

--- ansible/roles/twas_config/scripts/enableCustomProps.py
+++ ansible/roles/twas_config/scripts/enableCustomProps.py
@@ -13,6 +13,13 @@ print "Starting enable Pen test custom props script..."
 
 #--------------------------------------------------------------------
 # candidate to add to -> https://github.com/wsadminlib/wsadminlib/blob/master/bin/wsadminlib.py
+def modifyCookiesSessionName(nodename,servername,enabled,sessionname):
+    server_id = getServerByNodeAndName(nodename,servername)
+    sessionmanager = AdminConfig.list('SessionManager', server_id)
+    AdminConfig.modify(sessionmanager, [['enableCookies',enabled]])
+    cookie_id = AdminConfig.list('Cookie',sessionmanager)
+    AdminConfig.modify(cookie_id,[['name', sessionname]])
+
 def setapiDiscovery( nodename, servername, apiDiscovery, ):
     """Sets whether to use the apiDiscovery"""
     m = "setapiDiscovery:"
@@ -21,7 +28,7 @@ def setapiDiscovery( nodename, servername, apiDiscovery, ):
     #sop(m,"server_id=%s " % ( repr(server_id), ))
     webcontainer = AdminConfig.list('WebContainer', server_id)
     #sop(m,"webcontainer=%s " % ( repr(webcontainer), ))
-    result = AdminConfig.modify(webcontainer, [['setapiDiscovery', setapiDiscovery]])
+    result = AdminConfig.modify(webcontainer, [['apiDiscovery', apiDiscovery]])
     #sop(m,"Exit. result=%s" % ( repr(result), ))
 
 cellName = AdminControl.getCell()
@@ -31,11 +38,31 @@ for (nodename,servername) in listServersOfType(None):
     # sometimes, dmgr has no type... who knows why
     if stype == 'APPLICATION_SERVER':
         # hard coded for Pentest setup https://ibm.ent.box.com/notes/846078834982
+        print("-- setting custom prop: com.ibm.ws.webcontainer.addStrictTransportSecurityHeader")
         setWebContainerCustomProperty(nodename,servername,'com.ibm.ws.webcontainer.addStrictTransportSecurityHeader','max-age=31536000; includeSubDomains')
+        print("-- setting custom prop: com.ibm.ws.webcontainer.disablexPoweredBy")
         setWebContainerCustomProperty(nodename,servername,'com.ibm.ws.webcontainer.disablexPoweredBy','true')
+        print("-- setting custom prop: trusthostheaderport")
         setWebContainerCustomProperty(nodename,servername,'trusthostheaderport','true')
+        print("-- setting custom prop: com.ibm.ws.webcontainer.extractHostHeaderPort")
         setWebContainerCustomProperty(nodename,servername,'com.ibm.ws.webcontainer.extractHostHeaderPort','true')
+        print("-- setting custom prop: apiDiscovery")
         setapiDiscovery( nodename, servername, 'true', )
+        print("-- setting CookieName: PTSESSIONID")
+        modifyCookiesSessionName( nodename, servername, 'true', 'PTSESSIONID' )
+
+        # example to add port
+        # ensureHostAlias( 'default_host', '*', '9080' )
+        DEBUG_SOP=1
+        getHostAliasID( 'admin_host', '*', '9043' )
+        getHostAliasID( 'default_host', '*', '9080' )
+        getHostAliasID( 'default_host', '*', '80' )
+        getHostAliasID( 'default_host', '*', '5060' )
+        getHostAliasID( 'default_host', '*', '9443' )
+        getHostAliasID( 'default_host', '*', '5061' )
+        getHostAliasID( 'admin_host', '*', '9060' )
+        getHostAliasID( 'admin_host', '*', '9043' )
+        DEBUG_SOP=0
 
 # singleSignOn SSL very close to what is needed
 #https://github.com/wsadminlib/wsadminlib/blob/c3913bac556094989ac5de40afbee772858f806c/bin/wsadminlib.py#L6880
