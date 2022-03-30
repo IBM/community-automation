20a489ba36da62b7c47f47f2710e96eadc1dfe1f
Steven Schder <schader@us.ibm.com>
2022-03-30 17:36:11 -0400

    updateCipherList.py

---
+++ ansible/roles/twas_config/scripts/updateCipherList.py
@@ -0,0 +1,45 @@
+#--------------------------------------------------------------------
+# WebSphere cipher adjustment
+#--------------------------------------------------------------------
+#
+#
+# To invoke the script, type:
+#   wsadmin -f updateCipherList.py
+# 2016-01-19 schader@us.ibm.com initial version
+#--------------------------------------------------------------------
+import sys
+execfile('wsadminlib.py')
+
+print "Starting updateCipherList.py script..."
+
+#--------------------------------------------------------------------
+
+cellName = AdminControl.getCell()
+# enable the TLS requirement for every SSL config in the env... UGH>
+#productVersion=AdminTask.getNodeBaseProductVersion(['-nodeName', 'dmgr'])
+#print "modifying WAS version: " + productVersion
+
+# get the list of nodes and loop on every one minus unmanaged nodes
+nodes = _splitlines(AdminConfig.list( 'Node' ))
+for node_id in nodes:
+        nodename = getNodeName(node_id)
+        managedNode=nodeHasServerOfType( nodename, 'NODE_AGENT' )
+        appNode=nodeHasServerOfType( nodename, 'APPLICATION_SERVER' )
+        dmgrNode=nodeHasServerOfType( nodename, 'DEPLOYMENT_MANAGER' ) 
+
+        if( dmgrNode ):
+          # [1/7/16 10:49:07:747 EST] SSL certificate and key management > SSL configurations > CellDefaultSSLSettings > Quality of protection (QoP) settings
+          print "-- Setting custom cipher for CellDefaultSSLSettings"
+            AdminTask.modifySSLConfig(['-alias', 'CellDefaultSSLSettings', '-scopeName','(cell):'+cellName,'-securityLevel','CUSTOM', '-enabledCiphers','SSL_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDHE_RSA_WITH_AES_256_GCM_SHA384 SSL_RSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_RSA_WITH_AES_256_GCM_SHA384'])
+          print "-- Setting custom cipher list for XDADefaultSSLSettings"
+            AdminTask.modifySSLConfig(['-alias', 'XDADefaultSSLSettings', '-scopeName','(cell):'+cellName,'-securityLevel','CUSTOM', '-enabledCiphers','SSL_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDHE_RSA_WITH_AES_256_GCM_SHA384 SSL_RSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_RSA_WITH_AES_256_GCM_SHA384'])
+        #endif
+        if( managedNode or appNode ):
+            print "-- Setting custom cipher list for node: " + nodename
+            AdminTask.modifySSLConfig(['-alias', 'NodeDefaultSSLSettings', '-scopeName','(cell):'+cellName+':(node):'+nodename,'-securityLevel','CUSTOM', '-enabledCiphers','SSL_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDHE_RSA_WITH_AES_256_GCM_SHA384 SSL_RSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_RSA_WITH_AES_256_GCM_SHA384'])
+        #endif
+#endfor
+print "--Saving...."
+AdminConfig.save()
+print "--completed..."
+
