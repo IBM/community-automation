835917cfb1006a49b93856608f62035f2f395486
Steven Schader <schader@us.ibm.com>
2022-10-14 17:43:10 -0400

    updated pentest cipher list
    Signed-off-by: Steven Schader <schader@us.ibm.com>

--- ansible/roles/twas_config/scripts/updateCipherList.py
+++ ansible/roles/twas_config/scripts/updateCipherList.py
@@ -29,13 +29,13 @@ for node_id in nodes:
 
         if( dmgrNode ):
             print "-- Setting custom cipher for CellDefaultSSLSettings"
-            AdminTask.modifySSLConfig(['-alias', 'CellDefaultSSLSettings', '-scopeName','(cell):'+cellName,'-securityLevel','CUSTOM', '-enabledCiphers','SSL_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDHE_RSA_WITH_AES_256_GCM_SHA384 SSL_RSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_RSA_WITH_AES_256_GCM_SHA384'])
+            AdminTask.modifySSLConfig(['-alias', 'CellDefaultSSLSettings', '-scopeName','(cell):'+cellName,'-securityLevel','CUSTOM', '-enabledCiphers','SSL_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDHE_RSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_RSA_WITH_AES_256_GCM_SHA384 TLS_AES_256_GCM_SHA384 TLS_AES_128_GCM_SHA256 TLS_CHACHA20_POLY1305_SHA256'])
             print "-- Setting custom cipher list for XDADefaultSSLSettings"
-            AdminTask.modifySSLConfig(['-alias', 'XDADefaultSSLSettings', '-scopeName','(cell):'+cellName,'-securityLevel','CUSTOM', '-enabledCiphers','SSL_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDHE_RSA_WITH_AES_256_GCM_SHA384 SSL_RSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_RSA_WITH_AES_256_GCM_SHA384'])
+            AdminTask.modifySSLConfig(['-alias', 'XDADefaultSSLSettings', '-scopeName','(cell):'+cellName,'-securityLevel','CUSTOM', '-enabledCiphers','SSL_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDHE_RSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_RSA_WITH_AES_256_GCM_SHA384 TLS_AES_256_GCM_SHA384 TLS_AES_128_GCM_SHA256 TLS_CHACHA20_POLY1305_SHA256'])
         #endif
         if( managedNode or appNode ):
             print "-- Setting custom cipher list for node: " + nodename
-            AdminTask.modifySSLConfig(['-alias', 'NodeDefaultSSLSettings', '-scopeName','(cell):'+cellName+':(node):'+nodename,'-securityLevel','CUSTOM', '-enabledCiphers','SSL_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDHE_RSA_WITH_AES_256_GCM_SHA384 SSL_RSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_RSA_WITH_AES_256_GCM_SHA384'])
+            AdminTask.modifySSLConfig(['-alias', 'NodeDefaultSSLSettings', '-scopeName','(cell):'+cellName+':(node):'+nodename,'-securityLevel','CUSTOM', '-enabledCiphers','SSL_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDHE_RSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_ECDSA_WITH_AES_256_GCM_SHA384 SSL_ECDH_RSA_WITH_AES_256_GCM_SHA384 TLS_AES_256_GCM_SHA384 TLS_AES_128_GCM_SHA256 TLS_CHACHA20_POLY1305_SHA256'])
         #endif
 #endfor
 print "--Saving...."
