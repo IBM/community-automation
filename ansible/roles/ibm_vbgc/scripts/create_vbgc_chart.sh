ee8a2a68e86a60e6a819b1701ad2feeb121f0a5f
Steven Schader <schader@us.ibm.com>
2025-01-29 16:49:12 -0600

    vbgc debug
    Signed-off-by: Steven Schader <schader@us.ibm.com>

--- ansible/roles/ibm_vbgc/scripts/create_vbgc_chart.sh
+++ ansible/roles/ibm_vbgc/scripts/create_vbgc_chart.sh
@@ -10,10 +10,7 @@ do
     mkdir -p $outputDir/IBM_GCMV
     t1="$(dirname $line)/gctmp"
     export JAVA_TOOL_OPTIONS="-Djava.io.tmpdir=$t1 -Dosgi.configuration.area=$t1 -Duser.home=$t1"
-    echo 'debug' >> /tmp/gcmv.debug.out
-    pwd >> /tmp/gcmv.debug.out
-    ls -l >> /tmp/gcmv.debug.out
-    $IBM_GCMV -data=$t1 -p ./gcmv.genheap.epf -f $line -o $outputDir/IBM_GCMV &> /tmp/gcmv.$$.out
+    $IBM_GCMV -data=$t1 -p ./gcmv.genheap.epf -f $line -o $outputDir/IBM_GCMV 
     rm -rf $line $t1
 done < vbgcfiles.txt
 # purge the temp dir's for the GCMV
