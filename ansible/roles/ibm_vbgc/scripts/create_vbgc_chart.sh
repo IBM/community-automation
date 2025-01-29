c75eabcec7525aeadff40c29ace9fbea53bd0af1
Steven Schader <schader@us.ibm.com>
2025-01-29 12:31:03 -0600

    vbgc

--- ansible/roles/ibm_vbgc/scripts/create_vbgc_chart.sh
+++ ansible/roles/ibm_vbgc/scripts/create_vbgc_chart.sh
@@ -11,7 +11,7 @@ do
     t1="$(dirname $line)/gctmp"
     export JAVA_TOOL_OPTIONS="-Djava.io.tmpdir=$t1 -Dosgi.configuration.area=$t1 -Duser.home=$t1"
     $IBM_GCMV -data=$t1 -p gcmv.genheap.epf -f $line -o $outputDir/IBM_GCMV
-    rm -rf $line $t1 gcmv.genheap.epf
+    rm -rf $line $t1
 done < vbgcfiles.txt
 # purge the temp dir's for the GCMV
 find . -name gcmemoryvisualizerhtmlDataset1 -type d | xargs rm -rf
