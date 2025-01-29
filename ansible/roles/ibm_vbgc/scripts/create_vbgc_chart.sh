064bb8c00c79b271f51900c43137704c627314e6
Steven Schader <schader@us.ibm.com>
2025-01-29 14:29:26 -0600

    vbgc
    Signed-off-by: Steven Schader <schader@us.ibm.com>

--- ansible/roles/ibm_vbgc/scripts/create_vbgc_chart.sh
+++ ansible/roles/ibm_vbgc/scripts/create_vbgc_chart.sh
@@ -10,6 +10,9 @@ do
     mkdir -p $outputDir/IBM_GCMV
     t1="$(dirname $line)/gctmp"
     export JAVA_TOOL_OPTIONS="-Djava.io.tmpdir=$t1 -Dosgi.configuration.area=$t1 -Duser.home=$t1"
+    echo 'debug'
+    pwd
+    ls -l
     $IBM_GCMV -data=$t1 -p gcmv.genheap.epf -f $line -o $outputDir/IBM_GCMV
     rm -rf $line $t1
 done < vbgcfiles.txt
