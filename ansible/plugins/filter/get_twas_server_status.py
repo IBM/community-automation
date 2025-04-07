9c78fe0b7d50796f972c92584bd8a61421c792a9
Steven Schader <schader@us.ibm.com>
2025-04-07 16:18:49 -0500

    collection filter location
    Signed-off-by: Steven Schader <schader@us.ibm.com>

--- ansible/plugins/filter/get_twas_server_status.py
+++ ansible/plugins/filter/get_twas_server_status.py
@@ -3,9 +3,9 @@ import re
 class FilterModule(object):
     def filters(self):
         return {
-            'get_twas_server_status': self.get_twas_server_status,
+            'get_twas_server_names': self.get_twas_server_name,
         }
 
-    def get_twas_server_status(self, data):
+    def get_twas_server_name(self, data):
         m = re.search('"([^"]*)"', data)
         return m.group(1)
